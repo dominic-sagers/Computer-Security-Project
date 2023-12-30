6daca1314e0ded1954f984d54dcd8c8a3d7905a8
SofiaOzhogina <91745567+SofiaOzhogina@users.noreply.github.com>
2023-12-30 21:50:42 +0100

    proper delay

--- flaskServer.py
+++ flaskServer.py
@@ -57,6 +57,8 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
+    time.sleep(3) # A 3 second delay is implemented 
+
     # Check if the credentials are valid
     error_message, status = process_login(username, password)
     
